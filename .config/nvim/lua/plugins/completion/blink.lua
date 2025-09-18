025d4a6a1110a6215ffef1adadac51949d059883
R2m1liA <15258427350@163.com>
2025-09-18 13:14:59 +0800

    更新nvim配置
    - 调整nvim配置文件格式

--- .config/nvim/lua/plugins/completion/blink.lua
+++ .config/nvim/lua/plugins/completion/blink.lua
@@ -1,115 +1,115 @@
 return {
-	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
-	dependencies = { "rafamadriz/friendly-snippets" },
+  "saghen/blink.cmp",
+  -- optional: provides snippets for the snippet source
+  dependencies = { "rafamadriz/friendly-snippets" },
 
-	-- use a release tag to download pre-built binaries
-	version = "1.*",
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
+  -- use a release tag to download pre-built binaries
+  version = "1.*",
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
 
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
-	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = {
-			preset = "default",
-			["<A-j>"] = {
-				function(cmp)
-					return cmp.select_next({ auto_insert = false })
-				end,
-				"fallback",
-			},
-			["<A-k>"] = {
-				function(cmp)
-					return cmp.select_prev({ auto_insert = false })
-				end,
-				"fallback",
-			},
-			["<CR>"] = {
-				function(cmp)
-					return cmp.accept()
-				end,
-				"fallback",
-			},
-		},
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = {
+      preset = "default",
+      ["<A-j>"] = {
+        function(cmp)
+          return cmp.select_next({ auto_insert = false })
+        end,
+        "fallback",
+      },
+      ["<A-k>"] = {
+        function(cmp)
+          return cmp.select_prev({ auto_insert = false })
+        end,
+        "fallback",
+      },
+      ["<CR>"] = {
+        function(cmp)
+          return cmp.accept()
+        end,
+        "fallback",
+      },
+    },
 
-		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "mono",
-		},
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = "mono",
+    },
 
-		-- (Default) Only show the documentation popup when manually triggered
-		completion = {
-			accept = { auto_brackets = { enabled = true } },
-			list = { selection = { preselect = true, auto_insert = false } },
-			menu = {
-				border = "rounded",
-				max_height = 20,
-				draw = {
-					columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
-				},
-			},
-			documentation = {
-				auto_show = true,
-				-- Delay before showing the documentation window
-				auto_show_delay_ms = 200,
-				window = {
-					min_width = 10,
-					max_width = 120,
-					max_height = 20,
-					border = "rounded",
-					scrollbar = true,
-					direction_priority = {
-						menu_north = { "e", "w", "n", "s" },
-						menu_south = { "e", "w", "s", "n" },
-					},
-				},
-			},
-			ghost_text = {
-				enabled = true,
-				-- Show the ghost text when an item has been selected
-			},
-		},
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = {
+      accept = { auto_brackets = { enabled = true } },
+      list = { selection = { preselect = true, auto_insert = false } },
+      menu = {
+        border = "rounded",
+        max_height = 20,
+        draw = {
+          columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        -- Delay before showing the documentation window
+        auto_show_delay_ms = 200,
+        window = {
+          min_width = 10,
+          max_width = 120,
+          max_height = 20,
+          border = "rounded",
+          scrollbar = true,
+          direction_priority = {
+            menu_north = { "e", "w", "n", "s" },
+            menu_south = { "e", "w", "s", "n" },
+          },
+        },
+      },
+      ghost_text = {
+        enabled = true,
+        -- Show the ghost text when an item has been selected
+      },
+    },
 
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
-		sources = {
-			default = { "lazydev", "i18n", "lsp", "path", "snippets", "buffer" },
-			providers = {
-				lazydev = {
-					name = "LazyDev",
-					module = "lazydev.integrations.blink",
-					-- make lazydev compeletions top priority
-					score_offset = 100,
-				},
-				i18n = {
-					name = "i18n",
-					module = "i18n.integration.blink_source",
-				},
-			},
-		},
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { "lazydev", "i18n", "lsp", "path", "snippets", "buffer" },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          -- make lazydev compeletions top priority
+          score_offset = 100,
+        },
+        i18n = {
+          name = "i18n",
+          module = "i18n.integration.blink_source",
+        },
+      },
+    },
 
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
-		fuzzy = { implementation = "prefer_rust_with_warning" },
-	},
-	opts_extend = { "sources.default" },
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+  },
+  opts_extend = { "sources.default" },
 }
--- .config/nvim/lua/plugins/tools/conform.lua
+++ .config/nvim/lua/plugins/tools/conform.lua
@@ -1,24 +1,24 @@
 return {
-	"stevearc/conform.nvim",
+  "stevearc/conform.nvim",
 
-	---@module "conform"
-	---@type conform.setupOpts
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			vue = { "prettier" },
-			json = { "prettier" },
-			html = { "prettier" },
-			css = { "prettier" },
-			scss = { "prettier" },
-			markdown = { "prettier" },
-		},
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
-		log_level = vim.log.levels.DEBUG,
-	},
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      vue = { "prettier" },
+      json = { "prettier" },
+      html = { "prettier" },
+      css = { "prettier" },
+      scss = { "prettier" },
+      markdown = { "prettier" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+    log_level = vim.log.levels.DEBUG,
+  },
 }
