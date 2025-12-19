4cac5c7be30e1fce445ebfb8dde97d05d1f30892
R2m1liA <15258427350@163.com>
2025-12-19 16:26:53 +0800

    feat(nvim): 添加highlight-color

--- .config/nvim/lua/plugins/completion/blink.lua
+++ .config/nvim/lua/plugins/completion/blink.lua
@@ -62,6 +62,34 @@ return {
         max_height = 20,
         draw = {
           columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+          components = {
+            -- customize the drawing of kind icons
+            kind_icon = {
+              text = function(ctx)
+                local icon = ctx.kind_icon
+                -- if LSP source, check for color derived from documentation
+                if ctx.item.source_name == "LSP" then
+                  local color_item = require("nvim-highlight-colors").format(ctx.item.documentation, { kind = ctx.kind })
+                  if color_item and color_item.abbr ~= "" then
+                    icon = color_item.abbr
+                  end
+                end
+                return icon .. ctx.icon_gap
+              end,
+              highlight = function(ctx)
+                -- default highlight group
+                local highlight = "BlinkCmpKind" .. ctx.kind
+                -- if LSP source, check for color derived from documentation
+                if ctx.item.source_name == "LSP" then
+                  local color_item = require("nvim-highlight-colors").format(ctx.item.documentation, { kind = ctx.kind })
+                  if color_item and color_item.abbr_hl_group then
+                    highlight = color_item.abbr_hl_group
+                  end
+                end
+                return highlight
+              end,
+            }
+          }
         },
       },
       documentation = {
---
+++ .config/nvim/lua/plugins/ui/highlight-colors.lua
@@ -0,0 +1,8 @@
+return {
+  "brenoprata10/nvim-highlight-colors",
+  opts = {
+    render = 'virtual',
+
+    enable_tilwind = true,
+  }
+}
